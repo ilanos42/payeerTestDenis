175540e9f0643524d7bf09dab54faf1c609074e4
DenysNeto <dennetesin@gmail.com>
2022-07-02 15:01:55 +0300

    bbb

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello server is running").end();
-});
+// 👇️ "/home/john/Desktop/javascript"
+console.log("directory-name 👉️", __dirname);
+
+app.use(express.static(__dirname + "/assets"));
 
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log("Press Ctrl+C to quit.");
+});
+
+app.get("/", (req, res) => {
+  res.send("Homepage here");
 });
